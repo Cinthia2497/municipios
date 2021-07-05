d002a2a63bb745843893a664530e5d8507749423
Cinthia2497 <euceda_cinthia@yahoo.com>
2021-07-04 20:02:42 -0600

    muni

---
+++ muni.py
@@ -0,0 +1,15 @@
+cabecera  = {
+    "Lempira" : "Gracias" ,
+    "Intibuca" : "La esperanza" ,
+    "Choluteca" : "Choluteca" ,
+    "El paraiso" : "Yuscaran" ,
+    "Colon" : "Trujillo" ,
+    "Olancho" : "Juticalpa"
+}
+try:
+    print ("Cabeceras de Honduras")
+    print ("Ingrese Nombre del Departamento")
+    departamento = input("Ingresar depto.: ")
+    print("La Cebecera de {} es  {} .".format(departamento, format(cabecera[departamento],)))
+except KeyError:
+    print("El Pais no existe") 
